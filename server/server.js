c3f32b8a90604c9fc02f99ac10ffdd58306980be
AnanyaJindal1145 <ajindal1145@gmail.com>
2024-07-11 21:56:20 +0530

    server.js more backend

--- server/server.js
+++ server/server.js
@@ -5,10 +5,27 @@ const server=require('http').createServer(app);
 const io=require('socket.io')(server);
 const port=process.env.PORT || 3000;
 
+
+
+const {v4:uuidv4}=require('uuid');
+const {ExpressPeerServer}=require('peer');
+const peerServer=ExpressPeerServer(server,{
+    debug:true
+});
+//peer
+app.use('/peerjs',peerServer);
+//view engine
+app.set('view engine','ejs');
+//static files
+app.use(express.static('public'));
+
+
 //routes
 app.get('/',(req,res)=>{
     res.send('Server is running');
 });
+//middleware
+app.use(express.static('public'));
 //socket
 io.on('connection',(socket)=>{
     console.log('User connected');
@@ -20,6 +37,15 @@ io.on('connection',(socket)=>{
         console.log('User disconnected');
     });
 });
+//routes
+app.get('/room',(req,res)=>{
+    res.redirect(`/${uuidv4()}`);
+});
+app.get('/:room',(req,res)=>{
+    res.render('room',{roomId:req.params.room});
+});
+
+
 //server
 server.listen(port,()=>{
     console.log('Server is running on port '+port);
