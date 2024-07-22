9255cb62e20e534422e5d87d91fb6dda0d823830
AnanyaJindal1145 <ajindal1145@gmail.com>
2024-07-22 23:44:51 +0530

    hmmm

--- client/src/components/whiteboard.jsx
+++
@@ -1,6 +0,0 @@
-
-//full page whiteboard
-import React, { Component } from 'react';
-import { setAudio } from '../actions/audioActions';
-import { setScreen } from '../actions/screenActions';
-import 
