bf19a3c287557906b3cc5b6bd62db0ab01ea0fbd
AnanyaJindal1145 <ajindal1145@gmail.com>
2024-07-22 23:44:22 +0530

    hm

--- client/src/components/whiteboard.jsx
+++ client/src/components/whiteboard.jsx
@@ -6,6 +6,5 @@ import { setScreen } from '../actions/screenActions';
 import { setChat } from '../actions/chatActions';
 import { setWhiteboard } from '../actions/whiteboardActions';
 import { setVideoChat } from '../actions/videoChatActions';
-import { setAudioChat } from '../actions/audioChatActions';
-import { setScreenShare } from '../actions/screenShareActions';
+import { setAu
 
