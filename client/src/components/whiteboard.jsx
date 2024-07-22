6920c6afd5c3161deead71a0de4e0e31e0ddbd89
AnanyaJindal1145 <ajindal1145@gmail.com>
2024-07-22 23:44:37 +0530

    hmm

--- client/src/components/whiteboard.jsx
+++ client/src/components/whiteboard.jsx
@@ -3,8 +3,4 @@
 import React, { Component } from 'react';
 import { setAudio } from '../actions/audioActions';
 import { setScreen } from '../actions/screenActions';
-import { setChat } from '../actions/chatActions';
-import { setWhiteboard } from '../actions/whiteboardActions';
-import { setVideoChat } from '../actions/videoChatActions';
-import { setAu
-
+import 
