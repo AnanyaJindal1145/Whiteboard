73b5784054525266c2692b8fe113aaacdd5efce0
Ananya Jindal <ajindal1145@gmail.com>
2025-01-03 23:52:36 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const server=require('http').createServer(app);
 const io=require('socket.io')(server);
 const port=process.env.PORT || 3000;
 
-
+//somethings missing here
 
 const {v4:uuidv4}=require('uuid');
 const {ExpressPeerServer}=require('peer');
